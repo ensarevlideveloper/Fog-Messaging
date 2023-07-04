44cafec8a2941b75e2fb1984b47f2b40b9b15ee9
ensarevlideveloper <54323073+ensarevlideveloper@users.noreply.github.com>
2023-07-04 20:42:34 +0200

    Implemented Paranoid Pirate Pattern, Fake data exchange

--- client/client.py
+++ client/client.py
@@ -1,31 +1,51 @@
 import itertools
 import logging
 import sys
+#ZeroMQ
 import zmq
+#Fake library for data generation
+from faker import Faker
+from faker.providers import geo
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
+#ACK
+SIGNAL_ACK = b"\x04" #Signals Acknowledgement
 
+#EOL
+SIGNAL_EOL = b"\x03"  # Signals END OF LINE
+
+#How long to wait for an event
 REQUEST_TIMEOUT = 2500
+#Number of retries for a message
 REQUEST_RETRIES = 3
 SERVER_ENDPOINT = "tcp://localhost:5555"
 
 context = zmq.Context()
 
-logging.info("Connecting to server…")
+#Create a Faker instance and a geo provider for gps data
+fake = Faker()
+fake.add_provider(geo)
+
+
+logging.info("Connecting to Server…")
 client = context.socket(zmq.REQ)
 client.connect(SERVER_ENDPOINT)
 
 for sequence in itertools.count():
-    request = str(sequence).encode()
+    #Create
+    location = str(fake.latitude())+','+str(fake.longitude())
+    logging.info("Location is (%s)", location)
+    request = location.encode()
     logging.info("Sending (%s)", request)
     client.send(request)
-
+     
     retries_left = REQUEST_RETRIES
     while True:
         if (client.poll(REQUEST_TIMEOUT) & zmq.POLLIN) != 0:
-            reply = client.recv()
-            if int(reply) == sequence:
-                logging.info("Server replied OK (%s)", reply)
+            reply = client.recv_multipart()
+            logging.info("Server replied (%s)", reply)
+            if reply[0] == request and reply[-1] == SIGNAL_EOL:
+                logging.info("Temperature (%s) for location (%s)", reply[-2].decode(), reply[0].decode())
                 retries_left = REQUEST_RETRIES
                 break
             else:
